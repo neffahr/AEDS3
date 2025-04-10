0aca1a1be76a0341143805660902e7d099d6f893
neffahr <jpsousa837@gmail.com>
2025-04-10 19:30:25 -0300

    up

--- TP/BTree.java
+++ TP/BTree.java
@@ -7,34 +7,43 @@ import java.io.RandomAccessFile;
 
 public class BTree {
     protected int ordem;
-
     public static final String INDEX_FILE = "./arqs/btree_index.bin";
 
     private class Pagina {
         protected ArrayList<Integer> ids;
         protected ArrayList<Long> ends;
         protected ArrayList<Long> filhos;
-        protected long prox;
-        protected int num_elem;
-        protected int TAM_PAG;
+        protected byte num_elem;
 
-        public Pagina (ArrayList<Integer> ids, ArrayList<Long> ends, ArrayList<Long> filhos, long prox) {
+        public Pagina (ArrayList<Integer> ids, ArrayList<Long> ends, ArrayList<Long> filhos) {
             this.ids = ids;
             this.ends = ends;
             this.filhos = filhos;
-            this.prox = prox;
-            this.num_elem = ids.size();
-            this.TAM_PAG = 12 + (ids.size()*4) + (ends.size()*8) + (filhos.size()*8);
+            this.num_elem = (byte) ids.size();
         }
         public Pagina() {
             this.ids = new ArrayList<>(ordem-1);
             this.ends = new ArrayList<>(ordem-1);
             this.filhos = new ArrayList<>(ordem);
-            this.prox = -1;
             this.num_elem = 0;
-            this.TAM_PAG = 12 + (ids.size()*4) + (ends.size()*8) + (filhos.size()*8);
         }
-        
+        public int calcTamPag() {
+            /* 
+            num_elem (1 byte) + 
+            ids (4 para cada) + 
+            endereços (4 para cada) + 
+            endereço dos filhos (8 bytes para cada)
+            */
+            return 1 + ((ordem-1)*4) + ((ordem-1)*8) + ((ordem)*8); 
+        }
     }
+
+    
+
+    public BTree (int ordem) {
+        this.ordem = ordem;
+    }
+
+    
 }
 
